9e4a13a783d949d9b0d65021f0f2d73a48c5becb
mariomunozbayona <114985146+mariomunozbayona@users.noreply.github.com>
2025-11-23 22:01:01 +0100

    Delete public_html directory

--- public_html/controller/llistar_categories.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__.'/../model/connectaBD.php';
-require_once __DIR__.'/../model/categories.php';
-
-$categories = getCategories();
-
-include __DIR__.'/../views/llistar_categories.php';
-?>
